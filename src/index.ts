142e3075b4f973f2445d272f082b469c513444cf
SK-Fast <thummajil25580@gmail.com>
2022-02-24 20:07:06 +0700

    ESLINT

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { Intents, Client } from 'discord.js'
-import { config as configEnv } from 'dotenv'
-import firebaseUtils from './utils/firebaseUtils.js'
-import polyUtils from './utils/polyUtils.js'
-import log from './utils/logUtils.js'
-import { parse as parseCmd } from 'discord-command-parser'
+import {Intents, Client} from 'discord.js';
+import {config as configEnv} from 'dotenv';
+import firebaseUtils from './utils/firebaseUtils.js';
+import polyUtils from './utils/polyUtils.js';
+import log from './utils/logUtils.js';
+import {parse as parseCmd} from 'discord-command-parser';
 
-import onUserJoined from './resources/events/onUserJoined.js'
-import verifier from './resources/verify/verifier.js'
+import onUserJoined from './resources/events/onUserJoined.js';
+import verifier from './resources/verify/verifier.js';
 
-import commands from './export.js'
+import commands from './export.js';
 
 // Configurable Variables
-const prefix = '!poly' // Bot's Default prefix.
+const prefix = '!poly'; // Bot's Default prefix.
 
 // Initialize Discord Client
 const client = new Client({
@@ -19,84 +19,84 @@ const client = new Client({
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.DIRECT_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS
-  ]
-})
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+});
 
 // Configurate ENV files
-configEnv()
+configEnv();
 
 // Connect Application to Firebase
-firebaseUtils.init()
+firebaseUtils.init();
 
 // On Message sent
 client.on('messageCreate', async (message) => {
   // Parse the command
-  const parsed = parseCmd(message, prefix, { allowSpaceBeforeCommand: true })
+  const parsed = parseCmd(message, prefix, {allowSpaceBeforeCommand: true});
 
-  if (!parsed.success) return
+  if (!parsed.success) return;
 
-  if (parsed.command.toLowerCase() == "verify") {
-    commands["verify"](message, parsed.arguments)
-    return
+  if (parsed.command.toLowerCase() == 'verify') {
+    commands['verify'](message, parsed.arguments);
+    return;
   }
 
   if (message.guild === null) {
-    verifier(message, [], client)
-    return
+    verifier(message);
+    return;
   }
 
   // @ts-expect-error
-  const targetFunction = commands[parsed.command.toLowerCase()]
+  const targetFunction = commands[parsed.command.toLowerCase()];
 
   if (targetFunction) {
-    targetFunction(message, parsed.arguments, client)
+    targetFunction(message, parsed.arguments, client);
   }
-})
+});
 
 client.on('ready', () => {
-  log.logSuccess('Bot', 'Successfully Connected to Discord!')
-  //@ts-expect-error
-  client.user.setActivity(`${client.guilds.cache.reduce((a, guild) => a + guild.memberCount, 0)} Users | !poly verify`, { type: 'WATCHING' });
+  log.logSuccess('Bot', 'Successfully Connected to Discord!');
+  // @ts-expect-error
+  client.user.setActivity(`${client.guilds.cache.reduce((a, guild) => a + guild.memberCount, 0)} Users | !poly verify`, {type: 'WATCHING'});
 
   setInterval(function() {
-    //@ts-expect-error
-    client.user.setActivity(`${client.guilds.cache.reduce((a, guild) => a + guild.memberCount, 0)} Users | !poly verify`, { type: 'WATCHING' });
-  },60000)
-})
+    // @ts-expect-error
+    client.user.setActivity(`${client.guilds.cache.reduce((a, guild) => a + guild.memberCount, 0)} Users | !poly verify`, {type: 'WATCHING'});
+  }, 60000);
+});
 
 // Handle Errors without crashing process
 process.on('unhandledRejection', (reason, p) => {
-  console.log(p)
-})
+  console.log(p);
+});
 
-process.on('uncaughtException', err => {
-  console.log(err, 'Uncaught Exception thrown')
-})
+process.on('uncaughtException', (err) => {
+  console.log(err, 'Uncaught Exception thrown');
+});
 
 client.on('guildMemberAdd', async (member) => {
-  const isVerified = await firebaseUtils.isVerified(member.id)
+  const isVerified = await firebaseUtils.isVerified(member.id);
   if (isVerified === true) {
-    const verifiedRoleConfig = await firebaseUtils.getSpecificServerConfig(member.guild.id, 'verifiedRole')
+    const verifiedRoleConfig = await firebaseUtils.getSpecificServerConfig(member.guild.id, 'verifiedRole');
 
-    const setNicknameConfig = await firebaseUtils.getSpecificServerConfig(member.guild.id, 'setVerifiedNickname')
+    const setNicknameConfig = await firebaseUtils.getSpecificServerConfig(member.guild.id, 'setVerifiedNickname');
 
     if (verifiedRoleConfig) {
-      const role = member.guild.roles.cache.find(r => r.id === verifiedRoleConfig)
+      const role = member.guild.roles.cache.find((r) => r.id === verifiedRoleConfig);
 
-      //@ts-expect-error
-      member.roles.add(role)      
+      // @ts-expect-error
+      member.roles.add(role);
     }
     if (setNicknameConfig == true) {
-      const linkedUser = await firebaseUtils.getPolyUser(member.id)
-      const polyUser = await polyUtils.getUserInfoFromID(linkedUser.PolytoriaUserID)
+      const linkedUser = await firebaseUtils.getPolyUser(member.id);
+      const polyUser = await polyUtils.getUserInfoFromID(linkedUser.PolytoriaUserID);
 
       member.setNickname(polyUser.data.Username);
     }
-    return
+    return;
   }
-  onUserJoined(member, member.guild)
-})
+  onUserJoined(member, member.guild);
+});
 
-log.logNormal('Bot', 'Loggin in...')
-client.login(process.env.TOKEN)
+log.logNormal('Bot', 'Loggin in...');
+client.login(process.env.TOKEN);
